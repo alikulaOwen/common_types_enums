87327ac4f2e07576d4334212442d9ee0495c85a2
Caldewood Owen Alikula <alikulaowen@gmail.com>
2024-08-08 03:03:53 +0300

    end of types topc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
 //create an enum to classify web events
 
+use crate::List::*;
+
+enum List{
+    // Cons: Tuple struct that wraps an element and a pointer to the next node
+    Cons(u32, Box<List>),
+    // Nil: A node that signifies the end of the linked list
+    Nil,
+}
+
+impl List{
+    //c reats new empty list
+    fn new()-> List {
+        Nil
+    }
+
+    fn prepend(self, elem: u32)-> List{
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self)-> u32{
+        // `self` has to be matched, because the behavior of this method
+        // depends on the variant of `self`
+        // `self` has type `&List`, and `*self` has type `List`, matching on a
+        // concrete type `T` is preferred over a match on a reference `&T`
+        match *self {
+            // can't take ownership of the tail, because `self` is borrowed;
+            // instead take a reference to the tail
+            Cons(_, ref tail) => 1 + tail.len(),
+            // base case: an empty list has zero length
+            Nil => 0
+        }
+    }
+    fn stringify(&self)-> String{
+        match *self{
+            Cons(head, ref tail) => {
+                // `format!` is similar to `print!`, but returns a heap
+                // allocated string instead of printing to the console
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
+}
+
 enum  Events {
     // maybe this is like unit struct
     PageLoad,
@@ -27,6 +73,16 @@ enum Color {
     Black = 0x000000,
 }
 
+static LANGUAGE: &str = "PYTHON";
+const THRESHOLD: i32 = 10;
+
+
+
+
+
+
+
+
 // define a function for the enums
 
 fn inspect(event: Events) {
@@ -41,9 +97,23 @@ fn inspect(event: Events) {
     }
 }
 
+fn is_big(n: i32) ->bool {
+    n > THRESHOLD
+}
+
+type Second = u64;
+type Distance = u64;
 fn main() {
     println!("Hello, world!");
+    let mut list = List::new();
 
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+    list = list.prepend(4);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
    
     // `to_owned()` creates an owned `String` from a string slice.
 
@@ -56,7 +126,15 @@ fn main() {
 
     let clicked = Events::Click { x: 10, y: 20 };
     inspect(clicked);
-    
+    let n  = 16;
+
+    print!("{} ", n);
+    println!("{} ", n);
+    println!("{} ", n);
+    println!("{} ", THRESHOLD);
+    println!("{} ", LANGUAGE);
+    println!("{} is {}", n, if is_big(n) {"big"} else {"small"});
+
 
     println!("roses are #{:06x}", Color::Red as i32);
     println!("violets are #{:06x}", Color::Blue as i32);
@@ -66,4 +144,63 @@ fn main() {
 
     println!("zero is {}", Number::Zero as i32);
     println!("one is {}", Number::One as i32);
+
+
+    //  `variable binding ` mutablilty
+
+    let _immute = 1;
+    let mut mutable = 1;
+    println!("Before mutation: {}", mutable);
+
+    mutable += 1;
+
+    println!("After mutation : {}", mutable );
+    mutable += 1;
+
+    println!("After mutation 2 : {}", mutable);  
+
+    // `type conversions`
+
+    // rust has limited support for type coercion
+    // typical occurs in contexts like `Coverting fromn concrete type to a trait object ` and `Auto deferencing and deref coerciomn (ie converting &String to &str)`
+
+    let s:  &str = "Hello";
+    let string: String = String::from(s);
+
+    println!("{} world!", string); 
+    // `Type casting`
+
+    // this is where a programmer explicit define syntax in code for type conversion
+    // in rust we use `as` keyword
+
+    let x: i32 = 5;
+
+    let _y: u32 = x as u32;
+
+    //LITERALS
+    // numerical literals can be type annonated by adding type suffix
+    // ie 42 can be 132 by wring 42i32
+
+    let _x = 42i32;
+
+    //INferrence
+    let element = 5u8;
+    // creating an empty vector(growable array)
+    let mut vec = Vec::new();
+
+
+    vec.push(element);
+
+    println!("{:?}", vec);
+
+    //ALIASING
+    // type staements can be used to give a new name to exsting type.
+    // must have UpperCamelCase or compile willraise warning
+    // exceptions are only primitive types
+    let seconds: Second = 10 as u64;
+    let metres: Distance = 200 as u64;
+
+    println!("{} seconds and {} metres", seconds, metres)
+
+
 }
